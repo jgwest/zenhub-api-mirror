b75190189bfa365ecf37805d0a6c8c796d09233f
Jonathan West <jgwest@gmail.com>
2019-08-11 13:36:59 -0400

    Update artifact versions and shell scripts

--- resources/docker/build.sh
+++ resources/docker/build.sh
@@ -3,5 +3,5 @@
 export SCRIPT_LOCT=$( cd $( dirname $0 ); pwd )
 cd $SCRIPT_LOCT/../..
 
-mvn clean -f pom.xml
+mvn clean
 docker build -t zenhub-api-mirror --file=resources/docker/Dockerfile .
--- resources/docker/clean.sh
+++ resources/docker/clean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 docker rm -f zenhub-api-mirror-container > /dev/null 2>&1
 
-docker volume prune -f 
+echo "WARNING: Running 'docker volume prune', this may erase other unused volumes on your system."
+
+docker volume prune
 
